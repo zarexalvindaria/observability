9bb2b9ffff37d5028ce04f0768b4c43cb1c29785
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-11-07 19:17:34 +0800

    added spans to backend

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,3 +1,4 @@
+import opentracing.tracer
 from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
 import logging
@@ -34,28 +35,40 @@ mongo = PyMongo(app)
 
 tracer = init_tracer('backend')
 tracing = FlaskTracing(tracer, True, app)
+parent_span = tracing.get_span()
 
 
 @app.route('/')
 def homepage():
-    return "Hello World"
+    with opentracing.tracer.start_span("homepage", child_of=parent_span) as span:
+        response = {"message": "homepage"}
+        span.set_tag('message', response)
+        return "Hello World"
 
 
 @app.route('/api')
 def my_api():
-    answer = "something"
-    return jsonify(repsonse=answer)
+    with opentracing.tracer.start_span("homepage", child_of=parent_span) as span:
+        response = {"message": "api"}
+        span.set_tag("message", response)
+
+        answer = "something"
+        return jsonify(repsonse=answer)
 
 
 @app.route('/star', methods=['POST'])
 def add_star():
-    star = mongo.db.stars
-    name = request.json['name']
-    distance = request.json['distance']
-    star_id = star.insert({'name': name, 'distance': distance})
-    new_star = star.find_one({'_id': star_id})
-    output = {'name': new_star['name'], 'distance': new_star['distance']}
-    return jsonify({'result': output})
+    with opentracing.tracer.start_span("star", child_of=parent_span) as span:
+        try:
+            star = mongo.db.stars
+            name = request.json['name']
+            distance = request.json['distance']
+            star_id = star.insert({'name': name, 'distance': distance})
+            new_star = star.find_one({'_id': star_id})
+            output = {'name': new_star['name'], 'distance': new_star['distance']}
+            return jsonify({'result': output})
+        except:
+            span.set_tag("response", "Error: cannot access the database.")
 
 
 if __name__ == "__main__":
